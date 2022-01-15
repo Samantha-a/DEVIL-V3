cfcef44a07b4150827651c4d674dba210133ba64
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-01-15 16:56:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(𝔸𝕪𝕚𝕤𝕙𝕖𝕣𝕚", show_alert=True)
+        return await query.answer("𝔸𝕪𝕚𝕤𝕙𝕖𝕣𝕚", show_alert=True)
     try:
         offset = int(offset)
     except:
