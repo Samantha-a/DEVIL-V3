db8a9e9f8c75976f3eb27108cb4d246ce8fac73a
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-03-15 07:59:34 +0530

    Change

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,6 +5,93 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from config import Config
+from helpers.log import LOGGER
+from pyrogram import Client, errors
+from youtubesearchpython import VideosSearch
+from pyrogram.handlers import InlineQueryHandler
+from pyrogram.types import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+
+buttons = [
+            [
+                InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
+            ],
+            [
+                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/AsmSupport"),
+            ],
+            [
+                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/S1-BOTS/VideoPlayerBot/tree/alpha"),
+            ]
+         ]
+
+def get_cmd(dur):
+    if dur:
+        return "/play"
+    else:
+        return "/stream"
+
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    if query.query == "SAF_ONE":
+        answers.append(
+            InlineQueryResultPhoto(
+                title="Deploy Own Video Player Bot",
+                thumb_url="https://telegra.ph//file/3ed5eafa4a95960d33980.jpg",
+                photo_url="https://telegra.ph//file/3ed5eafa4a95960d33980.jpg",
+                caption=f"{Config.REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @AsmSupport 👑</b>",
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
+    string = query.query.lower().strip().rstrip()
+    if string == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text=("✍️ Type An Video Name !"),
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+    else:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "{} https://www.youtube.com/watch?v={}".format(get_cmd(v["duration"]), v["id"])
+                    ),
+                    thumb_url=v["thumbnails"][0]["url"]
+                )
+            )
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text=("❌ No Results Found !"),
+                switch_pm_parameter="",
+            )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
